fff5bdeeceb1fa5435daa52b0b6803e3161d8e00
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 01:06:58 +0530

    Nurse updated

--- src/com/hospital/model/Nurse.java
+++ src/com/hospital/model/Nurse.java
@@ -1,28 +1,38 @@
 package com.hospital.model;
 
 public class Nurse extends Person {
-    private String department;
+	private String department;
+	private ShiftType shiftTiming;
 
-    public Nurse(String name, String contactNumber, int age, String department) {
-        super(name, contactNumber, age);
-        this.department = department;
-    }
+	public Nurse(String name, String contactNumber, int age, String department, ShiftType shiftTiming) {
+		super(name, contactNumber, age);
+		this.department = department;
+		this.shiftTiming = shiftTiming;
+	}
 
-    @Override
-    public String getRole() {
-        return "Nurse";
-    }
+	@Override
+	public String getRole() {
+		return "Nurse";
+	}
 
-    public String getDepartment() {
-        return department;
-    }
+	public String getDepartment() {
+		return department;
+	}
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 
-    @Override
-    public String toString() {
-        return super.toString() + ", Nurse [department=" + department + "]";
-    }
+	public ShiftType getShiftTiming() {
+		return shiftTiming;
+	}
+
+	public void setShiftTiming(ShiftType shiftTiming) {
+		this.shiftTiming = shiftTiming;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Nurse [department=" + department + ", shiftTiming=" + shiftTiming + "]";
+	}
 }
