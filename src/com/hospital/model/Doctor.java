a2d9b1744044046a0e17d4ec7b3232f4ba01c4ab
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 01:05:01 +0530

    Doctor updated

--- src/com/hospital/model/Doctor.java
+++ src/com/hospital/model/Doctor.java
@@ -2,27 +2,29 @@ package com.hospital.model;
 
 public class Doctor extends Person {
     private String specialization;
+    private double consultationFee;
+    private int experienceYears;
 
-    public Doctor(String name, String contactNumber, int age, String specialization) {
+    public Doctor(String name, String contactNumber, int age, String specialization, double consultationFee, int experienceYears) {
         super(name, contactNumber, age);
         this.specialization = specialization;
+        this.consultationFee = consultationFee;
+        this.experienceYears = experienceYears;
     }
 
     @Override
-    public String getRole() {
-        return "Doctor";
-    }
+    public String getRole() { return "Doctor"; }
 
-    public String getSpecialization() {
-        return specialization;
-    }
-
-    public void setSpecialization(String specialization) {
-        this.specialization = specialization;
-    }
+    public String getSpecialization() { return specialization; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+    public double getConsultationFee() { return consultationFee; }
+    public void setConsultationFee(double consultationFee) { this.consultationFee = consultationFee; }
+    public int getExperienceYears() { return experienceYears; }
+    public void setExperienceYears(int experienceYears) { this.experienceYears = experienceYears; }
 
     @Override
     public String toString() {
-        return super.toString() + ", Doctor [specialization=" + specialization + "]";
+        return super.toString() + ", Doctor [specialization=" + specialization + ", consultationFee=" + consultationFee
+                + ", experienceYears=" + experienceYears + "]";
     }
 }
