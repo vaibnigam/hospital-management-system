55ae1b90ebaf7c1b7e0f5dcc07af3de8476e9b7e
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 02:09:49 +0530

    nurse repo

--- src/com/hospital/repository/NurseRepository.java
+++ src/com/hospital/repository/NurseRepository.java
@@ -1,5 +1,24 @@
 package com.hospital.repository;
 
-public class NurseRepository {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.hospital.model.Nurse;
+
+public class NurseRepository {
+    private Map<String, Nurse> nurses = new HashMap<>();
+
+    public void addNurse(Nurse nurse) {
+        nurses.put(nurse.getId(), nurse);
+    }
+
+    public Nurse findById(String id) {
+        return nurses.get(id);
+    }
+
+    public List<Nurse> getAllNurses() {
+        return new ArrayList<Nurse>(nurses.values());
+    }
 }
