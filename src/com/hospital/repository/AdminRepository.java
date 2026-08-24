89eb90dfb5aabe5da848f3c5d481e2ca2f85dc15
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 02:10:12 +0530

    admin repo

--- src/com/hospital/repository/AdminRepository.java
+++ src/com/hospital/repository/AdminRepository.java
@@ -1,5 +1,24 @@
 package com.hospital.repository;
 
-public class AdminRepository {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.hospital.model.Admin;
+
+public class AdminRepository {
+    private Map<String, Admin> admins = new HashMap<>();
+
+    public void addAdmin(Admin admin) {
+        admins.put(admin.getId(), admin);
+    }
+
+    public Admin findById(String id) {
+        return admins.get(id);
+    }
+
+    public List<Admin> getAllAdmins() {
+        return new ArrayList<Admin>(admins.values());
+    }
 }
