3c95a054a1f402c1232fa12f28e50d78a00e2e73
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 01:01:04 +0530

    Appointment

--- src/com/hospital/model/Appointment.java
+++ src/com/hospital/model/Appointment.java
@@ -1,5 +1,75 @@
 package com.hospital.model;
 
-public class Appointment {
+import java.time.LocalDateTime;
 
+public class Appointment {
+    private String appointmentId;
+    private String patientId;
+    private String doctorId;
+    private LocalDateTime appointmentDate;
+    private AppointmentStatus status;
+
+    public Appointment(String patientId, String doctorId, LocalDateTime appointmentDate) {
+        this.patientId = patientId;
+        this.doctorId = doctorId;
+        this.appointmentDate = appointmentDate;
+        this.status = AppointmentStatus.SCHEDULED;
+    }
+
+
+    public String getAppointmentId() {
+		return appointmentId;
+	}
+
+
+	public void setAppointmentId(String appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+
+
+	public String getPatientId() {
+		return patientId;
+	}
+
+
+	public void setPatientId(String patientId) {
+		this.patientId = patientId;
+	}
+
+
+	public String getDoctorId() {
+		return doctorId;
+	}
+
+
+	public void setDoctorId(String doctorId) {
+		this.doctorId = doctorId;
+	}
+
+
+	public LocalDateTime getAppointmentDate() {
+		return appointmentDate;
+	}
+
+
+	public void setAppointmentDate(LocalDateTime appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+
+	public AppointmentStatus getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(AppointmentStatus status) {
+		this.status = status;
+	}
+
+
+	@Override
+    public String toString() {
+        return "Appointment [appointmentId=" + appointmentId + ", patientId=" + patientId + ", doctorId=" + doctorId
+                + ", appointmentDate=" + appointmentDate + ", status=" + status + "]";
+    }
 }
