40fecfbff567b31ccd0a694439bcdc360876203c
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 02:09:13 +0530

    Doctor repo

--- src/com/hospital/repository/DoctorRepository.java
+++ src/com/hospital/repository/DoctorRepository.java
@@ -1,5 +1,24 @@
 package com.hospital.repository;
 
-public class DoctorRepository {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.hospital.model.Doctor;
+
+public class DoctorRepository {
+	private Map<String, Doctor> doctors = new HashMap<>();
+
+	public void addDoctor(Doctor doctor) {
+		doctors.put(doctor.getId(), doctor);
+	}
+
+	public Doctor findById(String id) {
+		return doctors.get(id);
+	}
+
+	public List<Doctor> getAllDoctors() {
+		return new ArrayList<Doctor>(doctors.values());
+	}
 }
