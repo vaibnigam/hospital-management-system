808a39ce3dad25ebe0d8c6f780703745b506572b
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 01:53:01 +0530

    patient repo

--- src/com/hospital/repository/PatientRepository.java
+++ src/com/hospital/repository/PatientRepository.java
@@ -1,5 +1,24 @@
 package com.hospital.repository;
 
-public class PatientRepository {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.hospital.model.Patient;
+
+public class PatientRepository {
+    private Map<String, Patient> patients = new HashMap<>();
+
+    public void addPatient(Patient patient) {
+    	patients.put(patient.getId(), patient);
+    }
+
+    public Patient findById(String id) {
+        return patients.get(id);
+    }
+
+    public List<Patient> getAllPatients() {
+        return new ArrayList<Patient>(patients.values());
+    }
 }
