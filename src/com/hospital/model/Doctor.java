1ab138fba9897191510463a06e29132b82abe7ac
Vaibhav Nigam <vaibnigam404@gmail.com>
2026-08-25 01:05:16 +0530

    Doctor

--- src/com/hospital/model/Doctor.java
+++ src/com/hospital/model/Doctor.java
@@ -1,30 +1,50 @@
 package com.hospital.model;
 
 public class Doctor extends Person {
-    private String specialization;
-    private double consultationFee;
-    private int experienceYears;
+	private String specialization;
+	private double consultationFee;
+	private int experienceYears;
 
-    public Doctor(String name, String contactNumber, int age, String specialization, double consultationFee, int experienceYears) {
-        super(name, contactNumber, age);
-        this.specialization = specialization;
-        this.consultationFee = consultationFee;
-        this.experienceYears = experienceYears;
-    }
+	public Doctor(String name, String contactNumber, int age, String specialization, double consultationFee,
+			int experienceYears) {
+		super(name, contactNumber, age);
+		this.specialization = specialization;
+		this.consultationFee = consultationFee;
+		this.experienceYears = experienceYears;
+	}
 
-    @Override
-    public String getRole() { return "Doctor"; }
+	@Override
+	public String getRole() {
+		return "Doctor";
+	}
 
-    public String getSpecialization() { return specialization; }
-    public void setSpecialization(String specialization) { this.specialization = specialization; }
-    public double getConsultationFee() { return consultationFee; }
-    public void setConsultationFee(double consultationFee) { this.consultationFee = consultationFee; }
-    public int getExperienceYears() { return experienceYears; }
-    public void setExperienceYears(int experienceYears) { this.experienceYears = experienceYears; }
+	public String getSpecialization() {
+		return specialization;
+	}
 
-    @Override
-    public String toString() {
-        return super.toString() + ", Doctor [specialization=" + specialization + ", consultationFee=" + consultationFee
-                + ", experienceYears=" + experienceYears + "]";
-    }
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+
+	public double getConsultationFee() {
+		return consultationFee;
+	}
+
+	public void setConsultationFee(double consultationFee) {
+		this.consultationFee = consultationFee;
+	}
+
+	public int getExperienceYears() {
+		return experienceYears;
+	}
+
+	public void setExperienceYears(int experienceYears) {
+		this.experienceYears = experienceYears;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + ", Doctor [specialization=" + specialization + ", consultationFee=" + consultationFee
+				+ ", experienceYears=" + experienceYears + "]";
+	}
 }
